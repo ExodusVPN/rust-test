8642467c7010f5a0b98911c9ccc4862835311efb
luozijun <luozijun.assistant@gmail.com>
2019-10-21 19:09:47 +0800

    update

--- src/main.rs
+++ src/main.rs
@@ -7,8 +7,8 @@ pub struct Output<W: Write> {
 }
 
 impl<W: Write> Output<W> {
-    pub fn new(inner: W) -> Self {
-        Self { inner: inner }
+    pub fn new(inner: W) -> Output {
+        Output { inner: inner }
     }
 
     pub fn into_inner(self) -> W {
